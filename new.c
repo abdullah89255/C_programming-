9f71d08aa4e169c95aef7884dbd818a011e908f9
@root <ayshakhan89255@gmail.com>
2025-03-06 01:27:17 +0600

    Add files via upload

---
+++ new.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int number = 25;
+    int age = 15;
+    char starr = 'a'; // Single character
+    float pi = 3.50;
+
+    printf("My number is %d \n", number);
+    printf("My age is %d \n", age);
+    printf("My character is %c \n", starr);
+    printf("The value of pi is %f \n", pi); // Limiting to 2 decimal places
+
+    return 0;
+}
